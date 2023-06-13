66103964964c725d1a2618608856fea90c404b80
Edwin141 <edvinas.mencleris@gmail.com>
2023-06-13 12:25:24 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Here we will use our components */}
+    </div>
+  );
+}
+
+export default App;
