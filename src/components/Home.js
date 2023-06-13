04a351e9df209c95ab5853082645de2d241b4679
Edwin141 <edvinas.mencleris@gmail.com>
2023-06-13 17:50:17 +0300

    Third commit, all the data is being fetched correctly

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,20 @@ import axios from 'axios';
 
 const Home = () => {
     const [coins, setCoins] = useState([]);
+    const [showMoreInfo, setShowMoreInfo] = useState(null);
+
+
+    const toggleMoreInfo = (id) => {
+        if (showMoreInfo === id) {
+          setShowMoreInfo(null);
+        } else {
+          setShowMoreInfo(id);
+        }
+      }
 
     useEffect(() => {
         axios
-            .get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
+            .get('/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
             .then(res => {
                 setCoins(res.data);
                 console.log(res.data);
@@ -23,7 +33,28 @@ const Home = () => {
                 return (
                     <div key={coin.id}>
                         <h4>{coin.name}</h4>
+                        <img 
+                        src={coin.image} 
+                        alt={coin.name} 
+                        onClick={() => toggleMoreInfo(coin.id)}
+                        style={{cursor: 'pointer'}}
+                        />
                         <p>Current Price: ${coin.current_price}</p>
+                        <p>Market Cap: ${coin.market_cap}</p>
+                        <p>24h Change: {coin.price_change_percentage_24h}%</p>
+                        {showMoreInfo === coin.id && (
+                            <div>
+                                <p>High 24h: ${coin.high_24h}</p>
+                                <p>Low 24h: ${coin.low_24h}</p>
+                                <p>Volume: ${coin.total_volume}</p>
+                                <p>Circulating Supply: {coin.circulating_supply}</p>
+                                <p>Total Supply: {coin.total_supply || 'Unknown'}</p>
+                                <p>Ath: ${coin.ath}</p>
+                                <p>Ath Change Percentage: {coin.ath_change_percentage}%</p>
+                                <p>Last Updated: {coin.last_updated}</p>
+                                {/* Add any other additional information here */}
+                            </div>
+                        )}
                     </div>
                 )
             })}
